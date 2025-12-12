05cbc14719e94716f6e8b1e39fcfb27b66cdb353
awakening-wind <1413710479@qq.com>
2025-12-12 13:02:32 +0800

    今日

---
+++ 数组12-7/数组12-7/shuzu.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//数组——一组相同类型元素的集合
+//int main()
+//{
+//	int arr[10] = { 1,2,3 };
+//	return 0;
+//}
+
+//二维数组
+//int main()
+//{
+//	int arr[3][4] = { 1,2,3,4,2,3,4,5,3,4,5,6 };
+//	int i, j;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("&arr[%d][%d]=%p  ",i,j,&arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+//程序员写代码要自己检查数组越界
+
+//冒泡排序
+void bubble_sort(int arr[],int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz-1; i++)
+	{
+		int j = 0;
+		for (j = 0;j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[] = {9,8,7,6,5,4,3,2,1,0};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
---
+++ 递归.c/递归.c/digui.c
@@ -0,0 +1,125 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//递归：大事化小
+//void print(int unsigned n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//
+//int main()
+//{
+//	int unsigned i = 0;
+//	scanf("%d", &i);
+//	print(i);
+//	return 0;
+//}
+
+//不创建临时变量，计算字符串大小
+
+//创建临时变量写法
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//不创建临时变量写法
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return 1 + my_strlen(str + 1);
+//	}
+//	else
+//		return 0;
+//
+//}
+//int main()
+//{
+//	char arr[] = "abc";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+//n的阶乘
+//int fac(int n)
+//{
+//	//递归
+//	/*if (n <= 1)
+//	{
+//		return 1;
+//	}
+//	else
+//		return n * fac(n - 1);
+//	*/
+//	//循环
+//	/*int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;*/
+//}
+//
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fac(n);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+
+
+//斐波那契数列
+//递归
+//int fie(int n)
+//{
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//		return (fie(n - 1) + fie(n - 2));
+//
+//}//计算结果正确，因栈溢出，效率低
+
+//迭代
+//int fie(int n)
+//{
+//	int i = 1;
+//	int j = 1;
+//	int k = 0;
+//	while (n > 2)
+//	{
+//		k = i + j;
+//		i = j;
+//		j = k;
+//		n--;
+//	}
+//	return k;
+//}
+//效率快但结果不对
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fie(n);
+//	printf("%d\n",ret);
+//	return 0;
+//}
